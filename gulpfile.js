6dc14fd13f04bb974fb0aeb2229323449191175d
Elizabeth Rogers <beth3346@gmail.com>
2019-02-08 20:53:12 -0600

    move mixins to a single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
-var gulp = require('gulp');
-var scsslint = require('gulp-scss-lint');
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+const scsslint = require('gulp-scss-lint');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('scsslint', function() {
-    return gulp.src('src/**/*.scss')
-        .pipe(scsslint());
+var paths = {
+  app: 'dist/',
+  css: 'dist/css/'
+};
+
+gulp.task('default', () => {
+  return gulp
+    .src('./*.scss')
+    .pipe(scsslint())
+    .pipe(plumber())
+    .pipe(
+      sass({
+        includePaths: ['node_modules']
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(cleanCSS({ debug: true }))
+    .pipe(gulp.dest(paths.css));
 });
-
-gulp.task('default', ['scsslint']);
